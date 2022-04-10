a43117cbb947a536049b4ebfaf5da121d090d9bf
José Antonio Pinto dos Santos <jose.santos@ps.otg.com.br>
2022-04-10 05:26:54 -0300

    feat: creating a request object of type struct

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -4,6 +4,13 @@ pragma solidity >=0.4.17 <0.9.0;
 
 contract Campaign {
 
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
