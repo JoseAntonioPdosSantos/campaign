1d4ec686991074f45e5174ee8add5f602a6e9700
José Antonio Pinto dos Santos <jose.santos@ps.otg.com.br>
2022-04-10 05:25:18 -0300

    feat: creating the list of approveres and a role that allows them to register and with a minimum contribution restriction

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -5,8 +5,16 @@ pragma solidity >=0.4.17 <0.9.0;
 contract Campaign {
 
     address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
 
-    constructor() {
+    constructor(uint minimum) {
         manager = msg.sender;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+        approvers.push(msg.sender);
     }
 }
